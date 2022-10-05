facc608fb517d22adb7501c1655cdd70493b2867
hayden <haydenburger43@gmail.com>
2022-10-05 14:04:25 -0500

    did the sidebar

--- haydens-react-portfolio/src/components/Sidebar/index.js
+++ haydens-react-portfolio/src/components/Sidebar/index.js
@@ -8,6 +8,31 @@ const Layout = () => (
         <img src={homeButton} alt="homeButton" />
 
     </link>
+    <nav>
+        <Navlink exact="true" activeclassname='active' to='/'>
+            <FontAwesomeIcon icon = {faHome} color="#8b8b91" />
+        </Navlink>
+
+        <Navlink exact="true" activeclassname='active' classname="about-link" to='/about'>
+            <FontAwesomeIcon icon = {faUser} color="#8b8b91" />
+        </Navlink>
+
+        <Navlink exact="true" activeclassname='active' className="contact-link" to='/contact'>
+            <FontAwesomeIcon icon = {faEnvelope} color="#8b8b91" />
+        </Navlink>
+    </nav>
+    <ul>
+        <li>
+            <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/hayden-carr-a27433227/'>
+                <FontAwesomeIcon icon={faLinkedin} color="#8b8b91" />
+            </a>
+        </li>
+        <li>
+            <a target="_blank" rel='noreferrer' href='https://github.com/haydenburger43'>
+                <FontAwesomeIcon icon={faGithub} color="#8b8b91" />
+            </a>
+        </li>
+    </ul>
 </div>
 )
 
