a80708a825850526f9643c3a51fc3c758cda12fa
hayden <haydenburger43@gmail.com>
2022-10-07 14:15:54 -0500

    added a contact page

--- haydens-react-portfolio/src/components/contact/index.js
+++ haydens-react-portfolio/src/components/contact/index.js
@@ -1,5 +1,40 @@
 import './index.scss'
 
-const Contact = () => 'text'
+const Contact = () => {
+    return(
+        <>
+            <div className='container contact-page'>
+                <h1>
+                    Contact Me
+                </h1>
+                <div>
+                    <form>
+                        <ul>
+                            <li className='half'>
+                                <input type="text" name="name" placeholder="Name" required />
+                            </li>
+                            
+                            <li className='half'>
+                                <input type="email" name="email" placeholder="Email" required />
+                            </li>
+
+                            <li>
+                                <input type="text" name="subject" placeholder="Subject" required />
+                            </li>
+
+                            <li>
+                                <textarea placeholder='Message' name='message' required ></textarea>
+                            </li>
+
+                            <li>
+                                <input type="submit" className='flat-button' value="SEND"></input>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default Contact
