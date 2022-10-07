cb77624e00b772641115d4a4f927b51331b6b26c
hayden <haydenburger43@gmail.com>
2022-10-07 14:18:42 -0500

    pushing

--- haydens-react-portfolio/src/App.js
+++ haydens-react-portfolio/src/App.js
@@ -1,12 +1,19 @@
 import './App.scss';
 import {Routes} from 'react-router-dom';
 import Layout from './components/layout';
+import Home from './components/home';
+import About from './components/About';
+import Contact from './components/contact';
 
 function App() {
   return (
     <>
     <Routes>
-      <route path="/" element={<layout />} ></route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Routes>
     </>
   );
---
+++ haydens-react-portfolio/src/components/About/index.js
@@ -0,0 +1,15 @@
+import './index.scss'
+
+const About = () => {
+    
+    return(
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>About Me</h1>
+                <p>Im Hayden, ive taken a bootcamp from UT Austin to become a fullstack software engineer. Im still learning and i am very confident on what i have already learned. I would be more than happy to show you the projects that ive worked on. </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
