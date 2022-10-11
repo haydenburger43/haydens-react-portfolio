e5f5108c864ccd3e0703335ba34776e01872af4d
hayden <haydenburger43@gmail.com>
2022-10-11 14:28:12 -0500

    fixing routes

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/haydens-react-portfolio/" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home/>} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Route>
     </Routes>
     </>
